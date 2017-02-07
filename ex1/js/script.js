39888920b29b99f551d5abc086e8a9697db1703c
Shrinivassab <shrinivassab@gmail.com>
2017-02-06 22:47:32 -0500

    First commit

---
+++ ex1/js/script.js
@@ -0,0 +1,4 @@
+d3.select("#someDiv").style("border", "5px darkgray dashed");
+	/*d3.select("#someDiv").style("background","red");*/
+	d3.select("#someDiv").attr("id", "newID");
+	d3.select("#someCheckbox").property("checked", true);
